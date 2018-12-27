aa2fbd93b24d7f9f56767607dd3dda5872e1c889
hexo <973960678@qq.com>
2018-12-27 16:29:12 +0800

    Site updated: 2018-12-27 16:29:11

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,18 +1,1 @@
-  /*页面载入完成后，创建复制按钮*/
-  !function (e, t, a) { 
-    /* code */
-    var initCopyCode = function(){
-      var copyHtml = '';
-      copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
-      //fa fa-globe可以去字体库替换自己想要的图标
-copyHtml += '  <i class="fa fa-clipboard"></i><span>copy</span>';
-      copyHtml += '</button>';
-      $(".highlight .code pre").before(copyHtml);
-      new ClipboardJS('.btn-copy', {
-          target: function(trigger) {
-              return trigger.nextElementSibling;
-          }
-      });
-    }
-    initCopyCode();
-  }(window, document);
+window,document,$(".highlight .code pre").before('<button class="btn-copy" data-clipboard-snippet="">  <i class="fa fa-clipboard"></i><span>copy</span></button>'),new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}});
